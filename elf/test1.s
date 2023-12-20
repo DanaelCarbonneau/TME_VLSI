e12ef40e606b89d27f77ff7a19add3bfd2985413
Millouze <74658025+Millouze@users.noreply.github.com>
2023-12-20 17:21:42 +0100

    Asm de test

---
+++ elf/test1.s
@@ -0,0 +1,31 @@
+/*----------------------------------------------------------------
+//           Mon premier programme                              //
+----------------------------------------------------------------*/
+    .text
+    .globl    _start
+_start:
+	nop
+	nop
+	mov r1, #1 
+	add r0, r1, r1
+	subs r0, r1, r0
+
+	adds r0, r0, r1
+
+	teq r0,r0
+	
+	mov r0, #0
+	addne r1, r0, #16
+	addeq r1,r0, #20
+	nop
+	beq _good
+	add r1,r0, #22
+	add r1,r0, #29
+	add r1,r0, #26
+
+_bad:
+	nop
+	nop
+_good:
+	nop
+	nop
